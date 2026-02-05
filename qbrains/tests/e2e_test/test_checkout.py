81aabe6fe0c959a31a70323f535dbc87d40eb6f1
olansolve <maulmalikib@gmail.com>
2026-02-05 10:39:38 +0700

    e2e.feat(checkout): add checkout test

---
+++ qbrains/tests/e2e_test/test_checkout.py
@@ -0,0 +1,46 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as p:
+    # 1.run the browser
+    browser = p.chromium.launch(headless=True)
+    # 2.make the context
+    context = browser.new_context()
+    # 3.make the page
+    page = context.new_page()
+    # 4.visit to https://practice.qabrains.com/ecommerce/login
+    page.goto('https://practice.qabrains.com/ecommerce/login')
+    # 5.fill Email
+    page.get_by_role('textbox', name='Email').fill('test@qabrains.com')
+    # 6.fill Password
+    page.get_by_role('textbox', name='Password').fill('Password123')
+    # 7.click the button 
+    page.get_by_role('button', name='Login').click()
+    # 8.assert login succesfully 
+    expect(page.get_by_text('Products')).to_be_visible()
+
+    # 9.add some product to cart
+    products = ['Sample Shoe Name',
+                'Sample Shirt Name',
+                'Sample Trouser Name']
+    for product in products:
+        page.locator('div.flex.flex-col.gap-3', has_text=product).get_by_role('button', name='Add to cart').click()
+    # 10.click cart
+    page.locator('span[role="button"].relative').click()
+    # 11.click checkout
+    page.get_by_role('button', name='Checkout').click()
+    # 12.fill form
+    page.get_by_placeholder('Ex. John').fill('mal')
+    page.get_by_placeholder('Ex. Doe').fill('ik')
+    # 13.click continue
+    page.get_by_role('button', name='Continue').click()
+    # 14.click Finish
+    page.get_by_role('button', name='Finish').click()
+    # 15. assert checkout succes
+    expect(page.get_by_role('button', name='Continue Shopping')).to_be_visible()
+    # 16.log out
+    page.get_by_role('button', name='test@qabrains.com').click()
+    page.get_by_role('button', name='Log out').click()
+    page.get_by_role('button', name='Logout').click()
+    expect(page.get_by_role('button', name='Login')).to_be_visible()
+
+    browser.close()
