87528d549ed6cb3573d40a91b365d4a7647738a0
olansolve <maulmalikib@gmail.com>
2026-03-30 12:40:47 +0700

    feat(login_page): add login_page

---
+++ toolshop/src/pages/login_page.py
@@ -0,0 +1,31 @@
+from base_page import BasePage
+
+class login(BasePage):
+    def __init__(self, page):
+        super().__init__(page)
+        self.navigate()
+        self.menu_button = "button[aria-label='Toggle navigation']"
+        
+    def fill_form(self, email=None, password=None):
+        self.page.click(self.menu_button)
+        self.page.get_by_role("link", name="Sign in").click()
+
+        if email:
+            self.page.get_by_test_id("email").fill(email)
+        else:
+            self.page.get_by_test_id("email").focus()
+
+        if password:
+            self.page.get_by_test_id("password").fill(password)
+        else:
+            self.page.get_by_test_id("password").focus()
+
+        self.page.get_by_test_id("login-submit").click()
+        
+    def click_logout(self):
+        # click menu button
+        self.page.click(self.menu_button)
+        # click account
+        self.page.get_by_role("link", name="Bob Smith").click()
+        # click sign out
+        self.page.get_by_role("link", name="Sign out").click()
